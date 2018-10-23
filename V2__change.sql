32e6d487c2f0f203837c44a47a8bf49775d0b14d
PRASANTH009 <prashaa@live.com>
2018-10-23 18:22:08 +0530

    Update V2__change.sql

--- V2__change.sql
+++ V2__change.sql
@@ -1,3 +1,4 @@
 insert into Flayway_test1 (ID, NAME) values (1, 'Axel');
 insert into Flayway_test1 (ID, NAME) values (2, 'Mr. Foo');
 insert into Flayway_test1 (ID, NAME) values (3, 'Ms. Bar');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
