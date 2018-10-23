0f9ae807b451f8fa63674a1cadc39a85400e450e
PRASANTH009 <prashaa@live.com>
2018-10-23 18:04:13 +0530

    Update V1__change.sql

--- V1__change.sql
+++ V1__change.sql
@@ -1,7 +1,4 @@
-CREATE TABLE FlywayTestVal (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255) 
+create table Flayway_test1 (
+    ID int not null,
+    NAME varchar(100) not null
 );
