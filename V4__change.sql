b13dfc08cbf38f4b07e7a45c4fbd8e71b986c7fc
PRASANTH009 <prashaa@live.com>
2018-10-24 15:42:25 +0530

    Update V4__change.sql

--- V4__change.sql
+++ V4__change.sql
@@ -1,21 +1,3 @@
--- -----------------------------------------------------
--- Add 2 new tables email and email_type.
--- Add some master record in email_type table.
--- -----------------------------------------------------
-
-create table email (
-  email_id          int not null auto_increment primary key,
-  email_address     varchar(100),
-  email_type        char(1) not null,
-  person_id         int not null
-);
-
-create table email_type (
-  email_type        char(1) not null primary key,
-  description       varchar(20)
-);
-
-insert into email_type (email_type, description)
-select 'P', 'primary'
-union all
-select 'W', 'work';
+insert into Flayway_test1 (ID, NAME) values (3, 'Axel1');
+insert into Flayway_test1 (ID, NAME) values (4, 'Mr. Fooo');
+insert into flayway_ (ID, NAME) values (3, 'Ms. Bar');
