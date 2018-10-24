1127eac1e6cbbc32c1861530870a9561981c2edb
PRASANTH009 <prashaa@live.com>
2018-10-24 11:31:25 +0530

    Update V3__insertinto.sql

--- V3__insertinto.sql
+++ V3__insertinto.sql
@@ -1,3 +1,3 @@
-insert into Flaywaytest1 (ID, NAME) values (4, 'Axel1');
-insert into Flaywaytest1 (ID, NAME) values (5, 'Mr. Foo0');
-insert into Flayway_test (ID, NAME) values (6, 'Ms. Bare');
+insert into flaywaytest1 (ID, NAME) values (4, 'Axel1');
+insert into flaywaytest1 (ID, NAME) values (5, 'Mr. Foo0');
+insert into flayway_test (ID, NAME) values (6, 'Ms. Bare');
