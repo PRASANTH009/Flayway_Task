e6c2ed91a522a410165918e3895fdda8ed48866d
PRASANTH009 <prashaa@live.com>
2018-10-24 16:08:24 +0530

    Update V5__change.sql

--- V5__change.sql
+++ V5__change.sql
@@ -1,9 +1,3 @@
--- -----------------------------------------------------
--- Alter email table and add constraint foreign keys of people and email_type table.
--- -----------------------------------------------------
-
-alter table email add constraint fk_person_email
-  foreign key (person_id) references people(person_id);
-
-alter table email add constraint fk_email_type
-  foreign key (email_type) references email_type(email_type);
+insert into Flayway_test2 (ID, NAME) values (1, 'Axel');
+insert into Flayway_test2 (ID, NAME) values (2, 'Mr. Foo');
+insert into Flayway_test2 (ID, NAME) values (3, 'Ms. Bar');
