d7b84776bc279bd4885d9cff05b80e78c6e521af
PRASANTH009 <prashaa@live.com>
2018-10-24 15:57:49 +0530

    Update V4__change.sql

--- V4__change.sql
+++ V4__change.sql
@@ -1,3 +1,4 @@
-insert into Flayway_test1 (ID, NAME) values (3, 'Axel1');
-insert into Flayway_test1 (ID, NAME) values (4, 'Mr. Fooo');
-insert into flayway_ (ID, NAME) values (3, 'Ms. Bar');
+create table Flayway_test1 (
+    ID int not null,
+    NAME varchar(100) not null
+);
